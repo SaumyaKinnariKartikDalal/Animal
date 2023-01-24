ffe18ceaaf77c2f14d28c7e904be9ecdd11d638d
SaumyaKinnariKartikDalal <98890702+SaumyaKinnariKartikDalal@users.noreply.github.com>
2023-01-24 21:46:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,3 +7,43 @@ function modelLoaded(){
     console.log("Model Loaded");
     classifier.classify(gotResults);
 }
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        r=Math.floor(Math.random()*255)+1;
+        g=Math.floor(Math.random()*255)+1;
+        b=Math.floor(Math.random()*255)+1;
+
+        document.getElementById("label").innerHTML = "I can Hear - "+ results[0].label;
+        document.getElementById("confidence").innerHTML = "I can Hear - "+ (results[0].confidence*100).toFixed(2)+"%";
+        document.getElementById("label").style.color = "rgb("+r+","+g+","+b+")";
+        document.getElementById("confidence").style.color = "rgb("+r+","+g+","+b+")";
+
+        A1 = document.getElementById("A1");
+
+        if (results[0].label == "Elephant"){
+            A1.src = "Elephant.png";
+
+        }
+        else if (results[0].label == "Dog"){
+            A1.src = "Dog.png";
+
+        }
+        else if (results[0].label == "Cat"){
+            A1.src = "Cat.png";
+
+        }
+        else if (results[0].label == "Tiger"){
+            A1.src = "Tiger.png";
+
+        }
+        else{
+            A1.src = "Ear.png";
+
+        }
+    }
+}
